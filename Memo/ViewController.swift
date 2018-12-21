379004ee4dd1a49d2b1b1558ad43272afd07051a
ryotakiyomoto <0424kiyomoto@gmail.com>
2018-12-22 07:56:06 +0900

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by 清本凌太 on 2018/12/22.
+//  Copyright © 2018年 清本凌太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
